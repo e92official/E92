47e0d71e45b202eafbc7ec6cfbc21622041f6cfe
e92official <63209404+e92official@users.noreply.github.com>
2023-11-04 01:58:23 +0000

    modified:   index.html 	new file:   script.js 	new file:   style.css

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('refreshBalance').addEventListener('click', function() {
+    // Insert logic here to fetch balance from the Lightning Node.
+    console.log('Refreshing balance...');
+});
+
+document.getElementById('sendPayment').addEventListener('click', function() {
+    // Insert logic here to send a payment.
+    console.log('Sending payment...');
+});
+
+document.getElementById('receivePayment').addEventListener('click', function() {
+    // Insert logic here to receive a payment.
+    console.log('Receiving payment...');
+});
