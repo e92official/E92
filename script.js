2457c02df765a53c805ae414e8e7be724078e48c
e92official <63209404+e92official@users.noreply.github.com>
2023-11-04 02:01:58 +0000

    modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,14 +1,57 @@
-document.getElementById('refreshBalance').addEventListener('click', function() {
-    // Insert logic here to fetch balance from the Lightning Node.
-    console.log('Refreshing balance...');
+// Simulated AJAX calls to a backend server
+function fetchBalance() {
+    // Simulate fetching balance from the backend
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log('Balance fetched.');
+            resolve(1000); // Simulated balance
+        }, 1000);
+    });
+}
+
+function sendPayment(amount, address) {
+    // Simulate sending payment to the backend
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (amount <= 0 || !address) {
+                console.error('Invalid payment details.');
+                reject('Invalid payment details.');
+                return;
+            }
+            console.log('Payment sent.');
+            resolve();
+        }, 1000);
+    });
+}
+
+function receivePayment() {
+    // Simulate receiving a payment
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log('Payment received.');
+            resolve(500); // Simulated received amount
+        }, 1000);
+    });
+}
+
+// Hook up buttons to actions
+document.getElementById('refreshBalance').addEventListener('click', async function() {
+    const balance = await fetchBalance();
+    document.getElementById('balanceAmount').textContent = balance;
 });
 
-document.getElementById('sendPayment').addEventListener('click', function() {
-    // Insert logic here to send a payment.
-    console.log('Sending payment...');
+document.getElementById('sendPayment').addEventListener('click', async function() {
+    try {
+        await sendPayment(100, 'exampleAddress'); // Simulated send amount and address
+        console.log('Payment sent successfully.');
+    } catch (error) {
+        console.error(error);
+    }
 });
 
-document.getElementById('receivePayment').addEventListener('click', function() {
-    // Insert logic here to receive a payment.
-    console.log('Receiving payment...');
+document.getElementById('receivePayment').addEventListener('click', async function() {
+    const amountReceived = await receivePayment();
+    const balanceElement = document.getElementById('balanceAmount');
+    const newBalance = parseInt(balanceElement.textContent) + amountReceived;
+    balanceElement.textContent = newBalance;
 });
